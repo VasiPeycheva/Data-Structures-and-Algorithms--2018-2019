effde359474dc1b229e273ceaf5177953bf86ce1
VasiPeycheva <vasilena.peicheva@gmail.com>
2018-11-07 21:24:53 +0200

    add solution to stack_to_queue task and balanced_parentheses task

---
+++ Practice_05--Stack-Queue/solutions/balanced_parentheses.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using std::stack;
+using std::string;
+using std::cout;
+using std::cin;
+
+
+/********** Task1 - Check for balanced parentheses *************/
+
+/**
+* help variables for isOpen(char) and match(char,char) functions
+*/
+char open[] = { '(', '[', '{' ,'<'};
+char close[] = { ')', ']', '}' , '>'};
+
+/**
+* check if the given symbol is open parentheses
+*/
+bool isOpen(char symbol) {
+	for (int i = 0; i < sizeof(open); i++) {
+		if (symbol == open[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+* It checks if both openBracket and closing bracket are same type parentheses
+* and they match
+*/
+bool match(char openBracket, char closingBracket) {
+	for (int i = 0; i < sizeof(close); i++) {
+		if (openBracket == open[i] && closingBracket == close[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+* Check if given string is correctly closed parentheses. To solve this task we
+* use one Stack. Each time if the symbol is open bracket, we push it into the
+* Stack. If it is closing bracket, we check if in the top of the stack we have
+* the same type bracket, but opening one. If two brackets do not match, we push
+* the symbol into the Stack. In the end we see if we have symbols in the stack.
+* If so, that means we have incorrect brackets order (not correctly closed)
+*/
+bool isCorrect(const string & str) {
+	stack<char> openedBrackets;
+	for (int i = 0; i < str.length(); i++) {
+		if (isOpen(str[i])) {
+			openedBrackets.push(str[i]);
+		}
+		else if (match(openedBrackets.top(), str[i])) {
+			openedBrackets.pop();
+		}
+		else {
+			break;
+		}
+	}
+	return openedBrackets.empty();
+}
+
---
+++ Practice_05--Stack-Queue/solutions/stack_to_queue.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+
+using std::stack;
+using std::cout;
+using std::cin;
+
+/********** Task2 - Stack to Queue *************/
+
+void stackToQueue()
+{
+	stack<int> front;
+	stack<int> rear;
+
+	short type;
+	unsigned int element;
+	unsigned int queries;
+
+	cin >> queries;
+	while (queries--)
+	{
+		cin >> type;
+		if (type == 1)
+		{
+			cin >> element;
+			rear.push(element);
+		}
+		else
+		{
+			if (front.empty())
+			{
+				while (!rear.empty())
+				{
+					front.push(rear.top());
+					rear.pop();
+				}
+			}
+			if (!front.empty())
+			{
+				if (type == 2)
+				{
+					front.pop();
+				}
+				if (type == 3)
+				{
+					cout << front.top() << '\n';
+				}  
+			}
+
+		}
+	}
+}
