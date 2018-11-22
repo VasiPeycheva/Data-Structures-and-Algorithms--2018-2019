d9e3ecc31ab22c6ad55f52f402244637a26337f0
IvanFilipov <vanaka1189@gmail.com>
2018-11-22 21:04:40 +0200

    unit tests for sorting : array elements beautifier

--- Practice_01--Unit-testing/SelectionSortTask/SortingAlgorithmsUnitTests/unittest.cpp
+++ Practice_01--Unit-testing/SelectionSortTask/SortingAlgorithmsUnitTests/unittest.cpp
@@ -12,8 +12,8 @@ namespace UnitTest
 
 		TEST_METHOD(BasicTest)
 		{
-			int expected[] = { 1,1,2,2,2,4,4,4,5,6,9,22,34 };
-			int arr[] = { 9,1,2,34,4,4,2,1,5,2,6,22,4 };
+			int expected[] = { 1, 1, 2, 2, 2, 4, 4, 4, 5, 6, 9, 22, 34 };
+			int arr[] = { 9, 1, 2, 34, 4, 4, 2, 1, 5, 2, 6, 22, 4 };
 			int size = sizeof(arr) / sizeof(int);
 			selectionSort(arr, size);
 			for (int i = 0; i < size; i++)
@@ -44,8 +44,8 @@ namespace UnitTest
 
 		TEST_METHOD(DescendingOrder)
 		{
-			int expected[] = { 0,1,2,3,4,5,6,7,8,9 };
-			int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+			int expected[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 			int size = sizeof(arr) / sizeof(int);
 			selectionSort(arr, size);
 			for (int i = 0; i < size; i++)
@@ -55,8 +55,8 @@ namespace UnitTest
 		}
 		TEST_METHOD(AscendingOrder)
 		{
-			int expected[] = { 0,1,2,3,4,5,6,7,8,9 };
-			int arr[] = { 0,1,2,3,4,5,6,7,8,9 };
+			int expected[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 			int size = sizeof(arr) / sizeof(int);
 			selectionSort(arr, size);
 			for (int i = 0; i < size; i++)
@@ -67,8 +67,8 @@ namespace UnitTest
 
 		TEST_METHOD(NegativeElement)
 		{
-			int expected[] = { -10,-4,-1,-1, 0, 2,34 };
-			int arr[] = { -10, 0,-1 ,34 ,-1 ,2, -4 };
+			int expected[] = { -10, -4, -1, -1, 0, 2, 34 };
+			int arr[] = { -10, 0, -1, 34, -1, 2, -4 };
 			int size = sizeof(arr) / sizeof(int);
 			selectionSort(arr, size);
 			for (int i = 0; i < size; i++)
